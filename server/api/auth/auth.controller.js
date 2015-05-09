555c0cd5780c3a779f3a61840972ef7a72a26904
chukkwagon <mcbride.charles4@gmail.com>
2015-05-08 19:58:08 -0500

    removed google auth

--- server/api/auth/auth.controller.js
+++ server/api/auth/auth.controller.js
@@ -8,12 +8,6 @@ exports.loginWithFacebookRedirect = passport.authenticate('facebook', {
   failureRedirect: '/welcome'
 });
 
-exports.loginWithGoogle = passport.authenticate('google', {scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email']});
-exports.loginWithGoogleRedirect = passport.authenticate('google', {
-  successRedirect: '/',
-  failureRedirect: '/welcome'
-});
-
 exports.checkAuthentication = function(req, res) {
   res.status(200).json(req.isAuthenticated());
 };
