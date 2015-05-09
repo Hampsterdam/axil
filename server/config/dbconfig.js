1cd6b47e985f29e16bd5d091032087a2898666ea
Christian Brandalise <christian.brandalise@gmail.com>
2015-05-09 11:11:42 -0500

    adds ionic app

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -4,10 +4,10 @@ module.exports = {
   client: 'pg',
   connection: {
     user: "postgres",
-    password: "crm0099",
+    password: "planes",
     database: "theseus-dev",
     port: 5432,
     host: "127.0.0.1"
   }
-  
+
 }
