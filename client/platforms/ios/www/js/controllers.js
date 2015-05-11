8e1f561d1dfaa02e59ee67ad03a84ed0d48a5212
Christian Brandalise <christian.brandalise@gmail.com>
2015-05-11 10:27:22 -0500

    new client

--- client/platforms/ios/www/js/controllers.js
+++ client/platforms/ios/www/js/controllers.js
@@ -1,58 +1,16 @@
-angular.module("phoenix.controllers", ["phoenix.services"])
+angular.module('starter.controllers', [])
+.controller("AuthCtrl", function($scope){
+	
+})
 
-.controller("LoginCtrl", function($scope, $cordovaOauth, $localStorage, $location) {
-
-    $scope.login = function() {
-        $cordovaOauth.facebook("1095011603846978", ["email", "read_stream", "user_website", "user_location", "user_relationships"]).then(function(result) {
-            $localStorage.accessToken = result.access_token;
-            $location.path("/tab.explore");
-        }, function(error) {
-            alert("There was a problem signing in!  See the console for logs");
-            console.log(error);
-        });
-    };
+.controller('ExploreCtrl', function($scope) {
 
 })
 
-.controller("ProfileCtrl", function($scope, $http, $localStorage, $location) {
-
-    $scope.init = function() {
-        if($localStorage.hasOwnProperty("accessToken") === true) {
-            $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: $localStorage.accessToken, fields: "id,name,gender,location,website,picture,relationship_status", format: "json" }}).then(function(result) {
-                $scope.profileData = result.data;
-            }, function(error) {
-                alert("There was a problem getting your profile.  Check the logs for details.");
-                console.log(error);
-            });
-        } else {
-            alert("Not signed in");
-            $location.path("/login");
-        }
-    };
+.controller('ProfileCtrl', function($scope) {
 
 })
 
-.controller("FeedCtrl", function($scope, $http, $localStorage, $location) {
+.controller('AddMediaCtrl', function($scope) {
 
-    $scope.init = function() {
-        if($localStorage.hasOwnProperty("accessToken") === true) {
-            $http.get("https://graph.facebook.com/v2.2/me/feed", { params: { access_token: $localStorage.accessToken, format: "json" }}).then(function(result) {
-                $scope.feedData = result.data.data;
-                $http.get("https://graph.facebook.com/v2.2/me", { params: { access_token: $localStorage.accessToken, fields: "picture", format: "json" }}).then(function(result) {
-                    $scope.feedData.myPicture = result.data.picture.data.url;
-                });
-            }, function(error) {
-                alert("There was a problem getting your profile.  Check the logs for details.");
-                console.log(error);
-            });
-        } else {
-            alert("Not signed in");
-            $location.path("/login");
-        }
-    };
-
-})
-
-.controller("UploadCtrl", function() {
-
-})
+});
