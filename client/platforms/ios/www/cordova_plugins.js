0a779461960b87e066e8bb10eaf7571641ccdace
Christian Brandalise <christian.brandalise@gmail.com>
2015-05-11 18:48:50 -0500

    fixes massive ios sim bug

--- client/platforms/ios/www/cordova_plugins.js
+++ client/platforms/ios/www/cordova_plugins.js
@@ -27,43 +27,12 @@ module.exports = [
         "clobbers": [
             "navigator.geolocation"
         ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.geolocation/www/Coordinates.js",
-        "id": "org.apache.cordova.geolocation.Coordinates",
-        "clobbers": [
-            "Coordinates"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.geolocation/www/PositionError.js",
-        "id": "org.apache.cordova.geolocation.PositionError",
-        "clobbers": [
-            "PositionError"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.geolocation/www/Position.js",
-        "id": "org.apache.cordova.geolocation.Position",
-        "clobbers": [
-            "Position"
-        ]
-    },
-    {
-        "file": "plugins/org.apache.cordova.geolocation/www/geolocation.js",
-        "id": "org.apache.cordova.geolocation.geolocation",
-        "clobbers": [
-            "navigator.geolocation"
-        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.ionic.keyboard": "1.0.4",
-    "org.apache.cordova.console": "0.2.13",
-    "org.apache.cordova.device": "0.3.0",
-    "cordova-plugin-geolocation": "1.0.0"
+    "cordova-plugin-geolocation": "1.0.1-dev"
 }
 // BOTTOM OF METADATA
 });
--- client/www/js/services.js
+++ client/www/js/services.js
@@ -20,176 +20,176 @@ angular.module('starter.services', [])
         });
     };
 
-    function addMedia (video_url, type, lat, lon, user_id) {
-        return $http({
-            method: 'POST',
-            url: 'http://localhost:9000/api/media',
-            data: {
-                uri: video_url,
-                type: type,
-                lat: lat,
-                lon: lon,
-                user_id
-            }
-        }).then(function(res) {
-            return res;
-        });
-    };
+    // function addMedia (video_url, type, lat, lon, user_id) {
+    //     return $http({
+    //         method: 'POST',
+    //         url: 'http://localhost:9000/api/media',
+    //         data: {
+    //             uri: video_url,
+    //             type: type,
+    //             lat: lat,
+    //             lon: lon,
+    //             user_id
+    //         }
+    //     }).then(function(res) {
+    //         return res;
+    //     });
+    // };
 
-    function updateMedia (media_id, tag) {
-        return $http({
-            method: 'PUT',
-            url: 'http://localhost:9000/api/media/' + media_id + '/' + tag,
-        }).then(function(res) {
-            return res;
-        });
-    };
+    // function updateMedia (media_id, tag) {
+    //     return $http({
+    //         method: 'PUT',
+    //         url: 'http://localhost:9000/api/media/' + media_id + '/' + tag,
+    //     }).then(function(res) {
+    //         return res;
+    //     });
+    // };
 
-    function deleteUniqueMedia (media_id) {
-        return $http({
-            method: 'DELETE',
-            url: 'http://localhost:9000/api/media/' + media_id,
-        }).then(function(res) {
-            return res;
-        });
-    };
+    // function deleteUniqueMedia (media_id) {
+    //     return $http({
+    //         method: 'DELETE',
+    //         url: 'http://localhost:9000/api/media/' + media_id,
+    //     }).then(function(res) {
+    //         return res;
+    //     });
+    // };
 
-    function likeMedia (media_id, user_id) {
-        return $http({
-            method: 'POST',
-            url: 'http://localhost:9000/api/media/' + media_id,
-            data: {
-                user_id: user_id
-            }
-        }).then(function(res) {
-            return res;
-        });
-    };
+    // function likeMedia (media_id, user_id) {
+    //     return $http({
+    //         method: 'POST',
+    //         url: 'http://localhost:9000/api/media/' + media_id,
+    //         data: {
+    //             user_id: user_id
+    //         }
+    //     }).then(function(res) {
+    //         return res;
+    //     });
+    // };
 
-    function unlikeMedia (media_id, user_id) {
-        return $http({
-            method: 'POST',
-            url: 'http://localhost:9000/api/media/' + media_id,
-            data: {
-                user_id: user_id
-            }
-        }).then(function(res) {
-            return res;
-        });
-    };
+    // function unlikeMedia (media_id, user_id) {
+    //     return $http({
+    //         method: 'POST',
+    //         url: 'http://localhost:9000/api/media/' + media_id,
+    //         data: {
+    //             user_id: user_id
+    //         }
+    //     }).then(function(res) {
+    //         return res;
+    //     });
+    // };
 
-    function getMediaByTag (tag) {
-        return $http({
-            method: 'GET',
-            url: 'http://localhost:9000/api/media/tags/' + tag
-        }).then(function (data) {
-            return data;
-        });
-    };
+    // function getMediaByTag (tag) {
+    //     return $http({
+    //         method: 'GET',
+    //         url: 'http://localhost:9000/api/media/tags/' + tag
+    //     }).then(function (data) {
+    //         return data;
+    //     });
+    // };
 
-    function getMediaByTime (time) {
-        return $http({
-            method: 'GET',
-            url: 'http://localhost:9000/api/media/time/' + time
-        }).then(function (data) {
-            return data;
-        });
-    };
+    // function getMediaByTime (time) {
+    //     return $http({
+    //         method: 'GET',
+    //         url: 'http://localhost:9000/api/media/time/' + time
+    //     }).then(function (data) {
+    //         return data;
+    //     });
+    // };
 
     return {
-        getAllMedia: getAllMedia,
-        getUniqueMedia: getUniqueMedia,
-        likeMedia: likeMedia,
-        unlikeMedia: unlikeMedia,
-        getMediaByTag: getMediaByTag,
-        getMediaByTime: getMediaByTime
+        getAllMedia: getAllMedia //DONT FORGET COMMA
+        // getUniqueMedia: getUniqueMedia,
+        // likeMedia: likeMedia,
+        // unlikeMedia: unlikeMedia,
+        // getMediaByTag: getMediaByTag,
+        // getMediaByTime: getMediaByTime
     }
 
 })
 
 .factory('UserFactory', function($http) {
 
-    function getAllUsers () {
-        return $http({
-            method: 'GET',
-            url: 'http://localhost:9000/api/users'
-        }).then(function (data) {
-            return data;
-        });
-    };
+//     function getAllUsers () {
+//         return $http({
+//             method: 'GET',
+//             url: 'http://localhost:9000/api/users'
+//         }).then(function (data) {
+//             return data;
+//         });
+//     };
 
-    function getUniqueUser (user_id) {
-        return $http({
-            method: 'GET',
-            url: 'http://localhost:9000/api/users/' + user_id
-        }).then(function (data){
-            return data;
-        })
-    }
+//     function getUniqueUser (user_id) {
+//         return $http({
+//             method: 'GET',
+//             url: 'http://localhost:9000/api/users/' + user_id
+//         }).then(function (data){
+//             return data;
+//         })
+//     }
 
-    function addUser(firstname, lastname, hometown, email){
-        return $http({
-            method: 'POST',
-            url: 'http://localhost:9000/api/users/',
-            data: {
-                firstname: firstname,
-                lastname: lastname,
-                hometown: hometown,
-                email: email
-            }
-        }).then(function (data){
-            return data; 
-        })
-    }
+//     function addUser(firstname, lastname, hometown, email){
+//         return $http({
+//             method: 'POST',
+//             url: 'http://localhost:9000/api/users/',
+//             data: {
+//                 firstname: firstname,
+//                 lastname: lastname,
+//                 hometown: hometown,
+//                 email: email
+//             }
+//         }).then(function (data){
+//             return data; 
+//         })
+//     }
 
-    function getFollowing(user_id){
-        return $http({
-            method: 'GET',
-            url: 'http://localhost:9000/' + user_id + '/following' 
-        }).then(function(data){
-            return data;
-        })
-    }
+//     function getFollowing(user_id){
+//         return $http({
+//             method: 'GET',
+//             url: 'http://localhost:9000/' + user_id + '/following' 
+//         }).then(function(data){
+//             return data;
+//         })
+//     }
 
-    function getFollowers(user_id){
-        return $http({
-            method: 'GET',
-            url: 'http://localhost:9000/' + user_id + '/followers',
-        }).then(function(data){
-            return data;
-        })
-    }
+//     function getFollowers(user_id){
+//         return $http({
+//             method: 'GET',
+//             url: 'http://localhost:9000/' + user_id + '/followers',
+//         }).then(function(data){
+//             return data;
+//         })
+//     }
 
-    function follow(user_id, friend_id){
-        return $http({
-            method: 'POST',
-            url: 'http://localhost:9000/' + user_id + '/following',
-            data: {
-                friend_id: friend_id
-            }
-        }).then(function(res){
-            return res;
-        })
-    }
+//     function follow(user_id, friend_id){
+//         return $http({
+//             method: 'POST',
+//             url: 'http://localhost:9000/' + user_id + '/following',
+//             data: {
+//                 friend_id: friend_id
+//             }
+//         }).then(function(res){
+//             return res;
+//         })
+//     }
 
-    function unfollow(user_id, friend_id){
-        return $http({
-            method: 'DELETE',
-            url: 'http://localhost:9000/' + user_id + '/following/' + friend_id,
-        }).then(function(res){
-            return res;
-        })
-    }
+//     function unfollow(user_id, friend_id){
+//         return $http({
+//             method: 'DELETE',
+//             url: 'http://localhost:9000/' + user_id + '/following/' + friend_id,
+//         }).then(function(res){
+//             return res;
+//         })
+//     }
 
-    return {
-        getAllUsers: getAllUsers,
-        getUniqueUser: getUniqueUser,
-        addUser: addUser,
-        getFollowing: getFollowing,
-        getFollowers: getFollowers,
-        follow: follow,
-        unfollow: unfollow
-    }
+    // return {
+    //     // getAllUsers: getAllUsers,
+    //     // getUniqueUser: getUniqueUser,
+    //     // addUser: addUser,
+    //     // getFollowing: getFollowing,
+    //     // getFollowers: getFollowers,
+    //     // follow: follow,
+    //     // unfollow: unfollow
+    // }
 })
 
 .factory('Helpers', function(){
