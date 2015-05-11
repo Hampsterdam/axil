69eac9615db5e8b37e4d28f2b06402e345d16ef8
chukkwagon <mcbride.charles4@gmail.com>
2015-05-11 16:33:41 -0500

    API updates to comply with REST

--- server/config/dbconfig.js
+++ server/config/dbconfig.js
@@ -4,7 +4,7 @@ module.exports = {
   client: 'pg',
   connection: {
     user: "postgres",
-    password: "planes",
+    password: "crm0099",
     database: "theseus-dev",
     port: 5432,
     host: "127.0.0.1"
