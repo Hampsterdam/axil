643a1245505c0da55b33a5fd98e4c15ca665a418
Christian Brandalise <christian.brandalise@gmail.com>
2015-05-12 11:41:10 -0500

    adds camera code

--- client/www/js/controllers.js
+++ client/www/js/controllers.js
@@ -32,6 +32,24 @@ angular.module('starter.controllers', [])
 
 })
 
-.controller('AddMediaCtrl', function($scope) {
-
+.controller('AddMediaCtrl', function($scope, $cordovaCamera) {
+  $scope.takePicture = function() {
+          var options = { 
+              quality : 75, 
+              destinationType : Camera.DestinationType.DATA_URL, 
+              sourceType : Camera.PictureSourceType.CAMERA, 
+              allowEdit : true,
+              encodingType: Camera.EncodingType.JPEG,
+              targetWidth: 300,
+              targetHeight: 300,
+              popoverOptions: CameraPopoverOptions,
+              saveToPhotoAlbum: true
+          };
+   
+          $cordovaCamera.getPicture(options).then(function(imageData) {
+              $scope.imgURI = "data:image/jpeg;base64," + imageData;
+          }, function(err) {
+              // An error occured. Show a message to the user
+          });
+      }
 });
