0e5c8269f96488dee474c99e9d016fd85b457284
Christian Brandalise <christian.brandalise@gmail.com>
2015-05-15 11:45:19 -0500

    server CORS changes

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ var jwtSecret = 'mysecret';
 var allowCrossDomain = function(req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, application/json");
 
     if('OPTIONS' == req.method){
         res.send(200);
