b7ffcdb7dbada8aaf0d10ea39bd4408266386ea0
chukkwagon <mcbride.charles4@gmail.com>
2015-05-25 14:19:53 -0500

    access control allow methods

--- server/app.js
+++ server/app.js
@@ -20,7 +20,7 @@ app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type,X-Requested-With,Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'DELETE');
+    res.header('Access-Control-Allow-Methods', 'DELETE', 'POST', 'GET', 'PUT');
     next();
 });
 
